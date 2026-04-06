e1166af3b5d7da3558459fe085e3ee7a1ab4cc8b
briyanii <e0310729@u.nus.edu>
2026-04-06 14:26:10 +0800

    Add comments and tool to search movies by filtering cast and genre

--- MetaRec-backend/agent/tools/entertainment.py
+++ MetaRec-backend/agent/tools/entertainment.py
@@ -1,4 +1,11 @@
+from typing import Optional
+
 async def search_music(query: str, ctx: any):
+    """
+    Lucene query. 
+    see: https://musicbrainz.org/doc/MusicBrainz_API/Search#Recording
+    """
+
     results = await ctx.musicbrainz.search_recordings(query)
     items = []
     for result in results:
@@ -20,7 +27,30 @@ async def search_tv_by_title(query: str, ctx: any):
         items.append(result['original_name'])
     return items
 
+async def search_movies_by_filter(
+        with_cast: Optional[str],
+        without_cast: Optional[str],
+        with_genres: Optional[str],
+        without_genres: Optional[str],
+        ctx: any,
+    ):
+    
+    results = await ctx.tmdb.search_movies_by_filter(
+        with_cast=with_cast,
+        without_cast=without_cast,
+        with_genres=with_genres,
+        without_genres=without_genres,
+    )
+    items = []
+    for result in results:
+        print(result)
+        items.append('placeholder movie')
+    return items
+
 async def search_books(query: str, ctx: any):
+    """
+    graphql query
+    """
     results = await ctx.hardcover.search_books_by_genre(query)
     items = []
     for result in results:
